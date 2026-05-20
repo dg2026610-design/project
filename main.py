ab59c165fee8a02deecef0c501930e9dd9761d88
dg2026602-hash <dg2026602@danggok.hs.kr>
2026-05-21 08:46:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+e = box(pos=vec(0, 0, 0), size=vec(10, 0.2, 10), color=color.white) 
+p = sphere(pos=vec(0, 5, 0), radius=0.5, color=color.orange)
+
+p.v = vec(0, 0, 0)      
+
+_gravity =  vec (0,-9.8,0)  
+_time = 0.02               
+speed = 0.05
+
+while p.pos.y >= (0.5+0.2):   
+    rate(100)    
+    p.v = p.v + _gravity * _time 
+    p.pos = p.pos + p.v * _time   
