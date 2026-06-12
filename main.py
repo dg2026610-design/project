0a08a185ef2e0e7cfca71809c162b67c027cace3
dg2026602-hash <dg2026602@danggok.hs.kr>
2026-06-12 09:36:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ Web VPython 3.2
 scene.autoscale = False
 scene.userspin = False
 
-\
+end = sphere(pos=vec(14.5,6, 0), radius=0.3, color=color.yellow)
 g  = box(pos=vec(5, -4.5, 0), size=vec(20.1, 10, 1.5), color=color.green)
-g2  = box(pos=vec(30, -4.5, 0), size=vec(20, 10, 1.5), color=color.green)
 sky = box(pos=vec(0,0,-1.2), size=vec(100,100,1), color=color.cyan)
 boxes = []
 for i in range(10) :
@@ -15,13 +14,13 @@ for i in range(10) :
 ball = sphere(pos=vec(0,10, 0), radius=0.5, color=color.orange)
 ball.v = vec(0, -1, 0)      
 gr =  vec(0,-9.8,0)  
-dt = 0.02
+dt = 0.01
 
     
 
 
 while True:
-    rate(100)
+    rate(50)
     scene.camera.follow(ball)
     k = keysdown()
     if 'd' in k:
@@ -31,6 +30,6 @@ while True:
     if ' ' in k:
         ball.pos.y += 1.1
     if ball.pos.y > (1):
-        rate(100)    
+        rate(80)    
         ball.v = ball.v + gr * dt
         ball.pos = ball.pos + ball.v * dt
